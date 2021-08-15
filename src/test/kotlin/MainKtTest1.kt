98da18bb1b448a5a80a95f4f01d3da7f7212f905
Ilya Permyakov <ilyap999@mail.ru>
2021-08-15 19:05:47 +0400

    first commit

---
+++ src/test/kotlin/MainKtTest1.kt
@@ -0,0 +1,72 @@
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class MainKtTest {
+
+    @Test
+    fun calculationCommis_Vkpay() {
+        val payType = PayType.Vkpay
+        val sumAmount = 100000
+        val amount = 50000
+
+        val result = calculationCommis(payType = payType, sumAmount = sumAmount, amount = amount)
+
+        assertEquals(0, result)
+    }
+
+    @Test
+    fun calculationCommis_Mastercard() {
+        val payType = PayType.Mastercard
+        val sumAmount = 10000
+        val amount = 62000
+
+        val result = calculationCommis(payType = payType, sumAmount = sumAmount, amount = amount)
+
+        assertEquals(2372, result)
+    }
+
+    @Test
+    fun calculationCommis_Maestro() {
+        val payType = PayType.Maestro
+        val sumAmount = 100000
+        val amount = 50000
+
+        val result = calculationCommis(payType = payType, sumAmount = sumAmount, amount = amount)
+
+        assertEquals(0, result)
+    }
+
+    @Test
+    fun calculationCommis_Visa() {
+        val payType = PayType.Visa
+        val sumAmount = 10000
+        val amount = 1000000
+
+        val result = calculationCommis(payType = payType, sumAmount = sumAmount, amount = amount)
+
+        assertEquals(7500, result)
+    }
+
+    @Test
+    fun calculationCommis_Mir() {
+        val payType = PayType.Mir
+        val sumAmount = 10000
+        val amount = 5000
+
+        val result = calculationCommis(payType = payType, sumAmount = sumAmount, amount = amount)
+
+        assertEquals(3500, result)
+    }
+
+    /*@Test
+    fun calculationCommis_DefaultParameters() {
+        //val payType = PayType.Visa
+        //val sumAmount = 10000
+        val amount = 82000
+
+        val result = calculationCommis(amount = amount)
+
+        assertEquals(0, result)
+    }*/
+}
